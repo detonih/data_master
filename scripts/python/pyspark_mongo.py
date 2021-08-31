64d1bcd49200926eb016fc49ee79087a8bb0f2b7
Henrique Detoni <detonihenrique@gmail.com>
2021-08-31 07:36:47 -0300

    envia dados do hive para o mongodb

--- scripts/python/pyspark_mongo.py
+++ scripts/python/pyspark_mongo.py
@@ -1,14 +1,23 @@
 import pymongo_spark
-from pyspark import SparkContext, SparkConf
+from pyspark.sql import SparkSession
 import os
 
 pymongo_spark.activate()
 
-MONGO_INITDB_ROOT_PASSWORD = os.getenv('MONGO_INITDB_ROOT_PASSWORD')
+MONGO_CONN_STRING = os.getenv('MONGO_CONN_STRING')
 
-conf = SparkConf().setAppName("pyspark test")
-sc = SparkContext(conf=conf)
+spark = SparkSession.builder \
+                    .appName("pyspark-mongo") \
+                    .enableHiveSupport() \
+                    .getOrCreate()
 
-rdd = sc.parallelize([1,2,3,4,5,6,7,8,9,10])
+sql = """
+SELECT * FROM enade.enade_tratado limit 10
+"""
+df = spark.sql(sql)
 
-rdd.saveToMongoDB(f'mongodb://root:{MONGO_INITDB_ROOT_PASSWORD}@172.21.0.1:27017/admin.enade')
+rdd = df.rdd.map(tuple)
+
+rdd.saveToMongoDB(MONGO_CONN_STRING)
+
+spark.stop()
